301e3a1063c87d54459a73d2f8f366354acc3622
R4ZXRN3T <rizelloyd@gmail.com>
2025-06-16 19:54:57 +0200

    added custom favicon

--- add/song/index.php
+++ add/song/index.php
@@ -7,6 +7,7 @@
     <title>BeatStream - add a song</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../addStyle.css" rel="stylesheet">
+    <link href="../../favicon.ico" rel="icon">
 </head>
 
 <body>
