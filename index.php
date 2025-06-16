6460fa1f22987c1ee838fa35c859af51060092c8
R4ZXRN3T <rizelloyd@gmail.com>
2025-06-16 19:08:07 +0200

    added style to main page

--- index.php
+++ index.php
@@ -2,68 +2,90 @@
 <html lang="en">
 
 <head>
-	<title>dies ist ein titel</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>add song</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
+<style>
+    body {
+        background-color: #f8f9fa;
+    }
+
+    table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+
+    th, td {
+        padding: 7px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+
+    th {
+        background-color: #343a40;
+        color: white;
+    }
+
+    tr:nth-child(even) {
+        background-color: #c7c7c7;
+    }
+</style>
+
 <body>
-	<h1>Ich bin ein header</h1>
-	<p>dies ist text</p>
 
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container-fluid">
-			<div class="collapse navbar-collapse myNavbar">
-				<ul class="navbar-nav">
-					<li class="nav-item"><a class="nav-link" href="../Arian_temp">Home</a></li>
-					<li class="nav-item"><a class="nav-link" href="./add/song">add songs</a></li>
-					<li class="nav-item"><a class="nav-link" href="./add/artist">add artist</a></li>
-					<li class="nav-item"><a class="nav-link" href="./add/user">add user</a></li>
-					<li class="nav-item"><a class="nav-link" href="./add/playlist">add playlist</a></li>
-					<li class="nav-item"><a class="nav-link" href="./add/album">add album</a></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse myNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="../beatstream">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="./add/song">Add content</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 
+<?php
+include("SongController.php");
+$songListe = SongController::getSongList();
+?>
+
+
+<table style="width:100%; font-family:segoe UI,serif;">
+    <colgroup>
+        <col span="9" style="background-color:lightgray">
+    </colgroup>
+    <tr>
+        <th style="width:11.1%;">Song ID</th>
+        <th style="width:11.1%;">Title</th>
+        <th style="width:11.1%;">Artists</th>
+        <th style="width:11.1%;">Genre</th>
+        <th style="width:11.1%;">Release Date</th>
+        <th style="width:11.1%;">Rating</th>
+        <th style="width:11.1%;">Song Length</th>
+        <th style="width:11.1%;">File Path</th>
+        <th style="width:11.1%;">Image Path</th>
+    </tr>
 	<?php
-	include("SongController.php");
-	$songListe = SongController::getSongList();
+	for ($i = 0; $i < count($songListe); $i++) {
+		?>
+        <tr>
+            <td><?php echo $songListe[$i]->getSongID() ?></td>
+            <td><?php echo $songListe[$i]->getTitle() ?></td>
+            <td><?php echo $songListe[$i]->getArtists() ?></td>
+            <td><?php echo $songListe[$i]->getGenre() ?></td>
+            <td><?php echo $songListe[$i]->getReleaseDate()->format('d.m.Y') ?></td>
+            <td><?php echo $songListe[$i]->getRating() ?></td>
+            <td><?php echo $songListe[$i]->getSongLength()->format('i:s') ?></td>
+            <td><?php echo $songListe[$i]->getFilePath() ?></td>
+            <td><?php echo $songListe[$i]->getImagePath() ?></td>
+        </tr>
+		<?php
+	}
 	?>
-
-
-
-	<table style="width:100%; font-family:segoe UI,serif;">
-		<colgroup>
-			<col span="9" style="background-color:lightgray">
-		</colgroup>
-		<tr>
-			<th style="width:11.1%;">Song ID</th>
-			<th style="width:11.1%;">Title</th>
-			<th style="width:11.1%;">Artists</th>
-			<th style="width:11.1%;">Genre</th>
-			<th style="width:11.1%;">Release Date</th>
-			<th style="width:11.1%;">Rating</th>
-			<th style="width:11.1%;">Song Length</th>
-			<th style="width:11.1%;">File Path</th>
-			<th style="width:11.1%;">Image Path</th>
-		</tr>
-		<?php
-		for ($i = 0; $i < count($songListe); $i++) {
-		?>
-			<tr>
-				<td><?php echo $songListe[$i]->getSongID() ?></td>
-				<td><?php echo $songListe[$i]->getTitle() ?></td>
-				<td><?php echo $songListe[$i]->getArtists() ?></td>
-				<td><?php echo $songListe[$i]->getGenre() ?></td>
-				<td><?php echo $songListe[$i]->getReleaseDate()->format('d.m.Y') ?></td>
-				<td><?php echo $songListe[$i]->getRating() ?></td>
-				<td><?php echo $songListe[$i]->getSongLength()->format('i:s') ?></td>
-				<td><?php echo $songListe[$i]->getFilePath() ?></td>
-				<td><?php echo $songListe[$i]->getImagePath() ?></td>
-			</tr>
-		<?php
-		}
-		?>
-	</table>
+</table>
 </body>
 
 </html>
