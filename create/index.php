65daf9ec0722248c159be0e50217e462c737ef85
Arian Pieroni <rizelloyd@gmail.com>
2025-06-21 18:40:07 +0200

    fixed create header
    Signed-off-by: Arian Pieroni <rizelloyd@gmail.com>

--- create/index.php
+++ create/index.php
@@ -9,7 +9,7 @@ if (!(isset($_SESSION['account_loggedin']) && $_SESSION['account_loggedin'] === 
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>BeatStream - create a playlist</title>
+	<title>BeatStream - Create</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link href="../favicon.ico" rel="icon">
 	<link href="../mainStyle.css" rel="stylesheet">
