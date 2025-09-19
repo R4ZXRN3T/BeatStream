3ed2b40570744975b14ac65226c46f9a172dd46f
Arian Pieroni <rizelloyd@gmail.com>
2025-09-19 16:53:51 +0200

    changed default string length to make call more convenient and added a proper password hashing function

--- Utils.php
+++ Utils.php
@@ -2,9 +2,15 @@
 
 class Utils
 {
-	public static function generateRandomString(int $length = 10, string $characterSet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'): string
+	public static function generateRandomString(int $length = 16, string $characterSet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'): string
 	{
 		$chars = str_split($characterSet);
 		return implode('', array_map(fn() => $chars[array_rand($chars)], range(1, $length)));
+	}
+
+	public static function hashPassword(string $password, string $salt, int $iterations = 200_000): string
+	{
+		for ($i = 0; $i < $iterations; $i++) $password = hash("sha256", $password . $salt);
+		return $password;
 	}
 }
