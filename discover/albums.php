81d0926b010f563c9f85e0a5944d83036e352cd9
Arian Pieroni <rizelloyd@gmail.com>
2025-09-23 19:18:05 +0200

    added player to all discover pages

--- discover/albums.php
+++ discover/albums.php
@@ -92,5 +92,6 @@ include( $GLOBALS['PROJECT_ROOT_DIR'] . "/components/topBar.php");
 	</div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<?php include($GLOBALS['PROJECT_ROOT_DIR'] . "/components/player.php"); ?>
 </body>
 </html>
